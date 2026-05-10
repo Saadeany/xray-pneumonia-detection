806e0092ea3e5698bddc6e5afda98c4ce99776a9
Yara <yaramohammmed11@gmail.com>
2026-05-11 02:55:09 +0300

    fix model DENSENET features import

--- src/model.py
+++ src/model.py
@@ -3,20 +3,20 @@ from torchvision.models import ResNet18_Weights
 import torch.nn as nn
 
 def get_model():
-
-    # Load ImageNet pre-trained weights
+    # 1. Load ImageNet pre-trained ResNet18
     model = models.resnet18(weights=ResNet18_Weights.DEFAULT)
-    #model = models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1)
 
+    # 2. Freeze all early layers so we don't destroy the pre-trained ImageNet edge-detectors
     for param in model.parameters():
         param.requires_grad = False
 
-    for param in model.features.denseblock4.parameters():
-        param.requires_grad = True
-    for param in model.features.norm5.parameters():
+    # 3. Unfreeze the final convolutional block (ResNet's equivalent to DenseNet's denseblock4)
+    # This allows the AI to learn specific lung textures (like fluid/consolidation)
+    for param in model.layer4.parameters():
         param.requires_grad = True
 
-    num_ftrs = model.classifier.in_features
-    model.classifier = nn.Linear(num_ftrs, 2)
+
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, 2)  # 2 Output classes: NORMAL and PNEUMONIA
 
     return model
